a9d943d4b6120b8588940cd1605eed0cd4faf941
ahmetberkinc <ahmetberkincoglu@gmail.com>
2023-11-16 15:26:57 +0300

    Add Missing Splash Step
    Missing init command added.

--- android/app/src/main/java/com/rnchallange/MainActivity.java
+++ android/app/src/main/java/com/rnchallange/MainActivity.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -21,6 +22,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.BootTheme);
     super.onCreate(null);
   }
 
